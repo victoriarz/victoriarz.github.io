1756f32d9c901b1367120ec5135bdc2c0785885a
Victoria Ruiz <v-ruiz@outlook.com>
2023-01-05 23:13:40 -0700

    more changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,13 @@
-// const myHeading = document.querySelector("h1");
-// myHeading.textContent = "Hello world!";
-// document.querySelector("html").addEventListener("click", function () {
-//     alert("Ouch! Stop poking me!");
-//   });
-const myImage = document.querySelector("img");
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/lemon-tree.jpeg") {
-    myImage.setAttribute("src", "images/sunflower.png");
-  } else {
-    myImage.setAttribute("src", "images/lemon-tree.jpeg");
-  }
-};
-let myButton = document.querySelector(".userid");
+// const myImage = document.querySelector("img");
+// myImage.onclick = () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/lemon-tree.jpeg") {
+//     myImage.setAttribute("src", "images/sunflower.png");
+//   } else {
+//     myImage.setAttribute("src", "images/lemon-tree.jpeg");
+//   }
+// };
+let myButton = document.querySelector(".user-id");
 let myHeading = document.querySelector("h1");
 
 function setUserName() {
@@ -29,22 +24,9 @@ function setUserName() {
     setUserName();
   } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Welcome, ${storedName}`;
+    myHeading.textContent = `Explore, ${storedName}`;
   }
-  myButton.onclick = () => {
+  myHeading.onclick = () => {
     setUserName();
   };
   
-
-  /**
-   * 
-   * 
-   * 
-   * example on how to select the <li> and highlight, based on URL location
-   */
-
-  //  url = url = window.location.href;
-  //  if (url.includes('index.html')) {
-  //      link = document.querySelector('body > div > ul > li:nth-child(2) > a');
-  //      link.style.color = '#a5cdbb';
-  //  }
